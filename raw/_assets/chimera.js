132d900bfdd87f08722c0132bfaff49d91e1d737
Naomi Guyer <adrexia@gmail.com>
2015-10-25 22:18:59 +1300

    Include assets

---
+++ raw/_assets/chimera.js
@@ -0,0 +1,23 @@
+function writeRandomImage()
+{
+	var i = Math.floor(Math.random()*35+1);
+	document.write('<img src="_assets/_img/photo' + i + '.png" border="0" width="272" height="281" alt="Photo ' + i + ' from previous Chimera"/>');
+}
+
+function validateForm(theForm)
+{
+	divValidationWarning = document.getElementById('ValidationWarning');
+	if (theForm.name != 'frmFeedback' && ((theForm.txtName.value == '') || (theForm.txtEmail.value == ''))) {
+		divValidationWarning.innerHTML = "Please enter your name and email address.";
+		divValidationWarning.style.visibility = "visible";
+		return false;
+	} else if (theForm.txtSpambot.value.toLowerCase() != "chimera") {
+		divValidationWarning.innerHTML = "Are you sure you're not a spambot?";
+		divValidationWarning.style.visibility="visible";
+		return false;
+	} else {
+		divValidationWarning.innerHTML = "";
+		divValidationWarning.style.visibility="hidden";
+		return true;
+	}
+}
